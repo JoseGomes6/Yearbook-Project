60222e38f3fbb3afc6462fdc35d1a7bf244a3440
Gomesyrn <josepedrogomes2010@hotmail.com>
2025-12-12 12:05:28 +0000

    Refined Login and Register layout

--- frontend/components/login.jsx
+++ frontend/components/login.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { FaUser, FaLock } from "react-icons/fa";
 
 export default function Login({ onSwitch, onLogin }) {
   return (
     <div className="container">
-      <h1>Login</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
+      <h1>Sign In</h1>
+
+      <div className="input-wrapper">
+        <FaUser className="input-icon" />
+        <input type="text" placeholder="Username" />
+      </div>
+
+      <div className="input-wrapper">
+        <FaLock className="input-icon" />
+        <input type="password" placeholder="Password" />
+      </div>
+
       <p style={{ textAlign: "left" }}>
         <span className="link-text" onClick={() => alert("Forgot Password!")}>
           Forgot Password?
         </span>
       </p>
+
       <button onClick={onLogin}>Sign In</button>
-      <p style={{ textAlign: "left", marginTop: "10px" }}>
+
+      <p style={{ textAlign: "left", marginTop: "150px" }}>
         Don't have an account?{" "}
         <span className="link-text" onClick={() => onSwitch("register")}>
           Create Account
--- frontend/components/register.jsx
+++ frontend/components/register.jsx
@@ -1,15 +1,41 @@
 import React from "react";
+import { FaUser, FaEnvelope, FaLock } from "react-icons/fa"; // precisa de npm install react-icons
 
 export default function Register({ onSwitch, onRegister }) {
   return (
     <div className="container">
-      <h1>Create Account</h1>
-      <input type="text" placeholder="Username" />
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
+      <div>
+        <h1>Create Account</h1>
+        <h3>Fill in your details to get started.</h3>
+      </div>
+
+      {/* Username */}
+      <div className="input-wrapper">
+        <FaUser className="input-icon" />
+        <input type="text" placeholder="Username" />
+      </div>
+
+      {/* Email */}
+      <div className="input-wrapper">
+        <FaEnvelope className="input-icon" />
+        <input type="email" placeholder="Email" />
+      </div>
+
+      {/* Password */}
+      <div className="input-wrapper">
+        <FaLock className="input-icon" />
+        <input type="password" placeholder="Password" />
+      </div>
+
+      {/* Confirm Password */}
+      <div className="input-wrapper">
+        <FaLock className="input-icon" />
+        <input type="password" placeholder="Confirm Password" />
+      </div>
+
       <button onClick={onRegister}>Get Started</button>
-      <p style={{ textAlign: "left", marginTop: "10px" }}>
+
+      <p style={{ textAlign: "left", marginTop: "65px" }}>
         Already have an account?{" "}
         <span className="link-text" onClick={() => onSwitch("login")}>
           Sign In
